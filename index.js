2ffdc93bb80e6388bd59940637fa1f5b3c69538f
ayebaishmo <ishmo255super@gmail.com>
2021-11-08 13:22:41 -0800

    Add DOM to side menu elements

---
+++ index.js
@@ -0,0 +1,15 @@
+var side_menu = document.querySelector('#side_menu');
+var logo_name = document.querySelector('#logo_name');
+var open_menu = document.querySelector('#open_menu');
+
+function openside_menu() {
+    side_menu.style.display = "block";
+    open_menu.style.display = "none";
+    logo_name.style.display = "none";
+}
+
+function closesideMenu() {
+    side_menu.style.display = "none";
+    open_menu.style.display = "block";
+    logo_name.style.display = "block";
+}
