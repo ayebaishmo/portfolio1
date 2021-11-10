72197891fa23bf62158889619856e01b21084438
ayebaishmo <ishmo255super@gmail.com>
2021-11-10 06:48:46 -0800

    Add background image in Dom

--- index.js
+++ index.js
@@ -83,9 +83,10 @@ projTitle.textContent = project[0].name;
 //create backgroundpicture
 const pic = document.createElement('picture');
 carddiv.appendChild(pic);
-const backImg = document.createElement('img');
-pic.appendChild(backImg);
-backImg.textContent = project[0].img; 
+carddiv.style.backgroundImage = "url('img/works_card.png')";
+// const backImg = document.createElement('img');
+// pic.appendChild(backImg);
+// backImg.textContent = project[0].img; 
 
 
 //create paragraph
@@ -103,61 +104,61 @@ listlangs.appendChild(buttonsLang);
 buttonsLang.textContent = project[0].langs[1];
 
 
-//last big button
-const bigButton = document.createElement('button');
-carddiv.appendChild(bigButton);
-bigButton.textContent = project[0].see;
+// //last big button
+// const bigButton = document.createElement('button');
+// carddiv.appendChild(bigButton);
+// bigButton.textContent = project[0].see;
 
 
-//popup background
-const popBack = document.createElement('div');
-carddiv.appendChild(popBack);
-popBack.className = 'projPopup';
-const popBackx = document.createElement('div');
-popBack.appendChild(popBackx);
-popBackx.className = 'popup_background';
+// //popup background
+// const popBack = document.createElement('div');
+// carddiv.appendChild(popBack);
+// popBack.className = 'projPopup';
+// const popBackx = document.createElement('div');
+// popBack.appendChild(popBackx);
+// popBackx.className = 'popup_background';
 
-//popup card 
-const card = document.createElement('div');
-popBack.appendChild(card);
-card.className = 'projDet';
+// //popup card 
+// const card = document.createElement('div');
+// popBack.appendChild(card);
+// card.className = 'projDet';
 
-//popup heading 
-const popHead = document.createElement('h4');
-card.appendChild(popHead);
-popHead.textContent = project[0].name;
+// //popup heading 
+// const popHead = document.createElement('h4');
+// card.appendChild(popHead);
+// popHead.textContent = project[0].name;
 
-//popclosebutton
-const popupButton = document.createElement('button');
-card.appendChild(popupButton);
-popupButton.className = 'close_popup';
+// //popclosebutton
+// const popupButton = document.createElement('button');
+// card.appendChild(popupButton);
+// popupButton.className = 'close_popup';
 
-//popLanguagues
-const popupLangs = document.createElement('ul');
-card.appendChild(popupLangs);
-popupLangs.className = 'projLanguages';
-const popLangsLi = document.createElement('li');
-popupLangs.appendChild(popLangsLi);
-const popliButtons = document.createElement('button');
-popLangsLi.appendChild(popliButtons);
-popliButtons.textContent = project[0].langs[0];
+// //popLanguagues
+// const popupLangs = document.createElement('ul');
+// card.appendChild(popupLangs);
+// popupLangs.className = 'projLanguages';
+// const popLangsLi = document.createElement('li');
+// popupLangs.appendChild(popLangsLi);
+// const popliButtons = document.createElement('button');
+// popLangsLi.appendChild(popliButtons);
+// popliButtons.textContent = project[0].langs[0];
 
-//popimg
-const popimg = document.createElement('img');
-card.appendChild(popimg);
-popimg.textContent = project[0].popImg;
+// //popimg
+// const popimg = document.createElement('img');
+// card.appendChild(popimg);
+// popimg.textContent = project[0].popImg;
 
-//poppara
-const popupPara = document.createElement('p');
-card.appendChild(popupPara);
-popupPara.textContent = project[0].popPara
+// //poppara
+// const popupPara = document.createElement('p');
+// card.appendChild(popupPara);
+// popupPara.textContent = project[0].popPara
 
-//poplastButtons
-const ulButton = document.createElement('ul');
-card.appendChild(ulButton);
-ulButton.className = 'see_buttons';
-const liButton = document.createElement('li');
-ulButton.appendChild(liButton);
-const poplastButtons = document.createElement('button');
-liButton.appendChild(poplastButtons);
-poplastButtons.textContent = project[0].popLastB;
+// //poplastButtons
+// const ulButton = document.createElement('ul');
+// card.appendChild(ulButton);
+// ulButton.className = 'see_buttons';
+// const liButton = document.createElement('li');
+// ulButton.appendChild(liButton);
+// const poplastButtons = document.createElement('button');
+// liButton.appendChild(poplastButtons);
+// poplastButtons.textContent = project[0].popLastB;
