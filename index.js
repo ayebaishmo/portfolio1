0ced71d5fff22c3808080b7048ace9e112c57b08
ayebaishmo <ishmo255super@gmail.com>
2021-11-11 00:11:30 -0800

    Fix lint errors

--- index.js
+++ index.js
@@ -148,63 +148,62 @@ for (let i = 0; i < project.length; i += 1) {
   carddiv.appendChild(pic);
   const backImg = document.createElement('img');
   pic.appendChild(backImg);
-  if (windowSize.matches) 
-    {
-      backImg.setAttribute('src', project[i].deskImg);
-    } else 
-    {
-      backImg.setAttribute('src', project[i].img);
-      backImg.removeAttribute('src', project[i].deskImg);
+  if (windowSize.matches)
+  {
+    backImg.setAttribute('src', project[i].deskImg);
+  } else 
+  {
+    backImg.setAttribute('src', project[i].img);
+    backImg.removeAttribute('src', project[i].deskImg);
     }
   // create paragraph
-  let para = document.createElement('p');
+  const para = document.createElement('p');
   carddiv.appendChild(para);
   para.textContent = project[i].desc;
-  
+
   // create buttons
-  let languages = document.createElement('ul');
+  const languages = document.createElement('ul');
   carddiv.appendChild(languages);
 
   for (let j = 0; j < project[i].langs.length; j += 1) {
-    let listlangs = document.createElement('li');
+    const listlangs = document.createElement('li');
     languages.appendChild(listlangs);
 
-    let buttonsLang = document.createElement('button');
+    const buttonsLang = document.createElement('button');
     listlangs.appendChild(buttonsLang);
     buttonsLang.textContent = project[i].langs[j];
   }
 
   // last big button
-  let bigButton = document.createElement('button');
+  const bigButton = document.createElement('button');
   carddiv.appendChild(bigButton);
   bigButton.textContent = 'See Project';
   bigButton.setAttribute('id', i.toString());
   bigButton.className = 'openPopupButton';
 }
 
-
-let id = 0
+const id = 0
 const popupWrapper = document.createElement('div')
 projCtn.appendChild(popupWrapper)
 
 function createPopup(id = 0) {
   // create popup div
-  let popBack = document.createElement('div');
+  const popBack = document.createElement('div');
   popupWrapper.appendChild(popBack);
   popBack.className = 'projPopup';
   
   // create background (surrounds card)
-  let popBackx = document.createElement('div');
+  const popBackx = document.createElement('div');
   popBack.appendChild(popBackx);
   popBackx.className = 'popup_background';
   
   // popup card
-  let card = document.createElement('div');
+  const card = document.createElement('div');
   popBack.appendChild(card);
   card.className = 'projDet';
   
   // popup heading 
-  let popHead = document.createElement('h4');
+  const popHead = document.createElement('h4');
   card.appendChild(popHead);
   if (windowSize.matches) {
       popHead.textContent = project[id].name;
@@ -213,7 +212,7 @@ function createPopup(id = 0) {
     }
   
   // popclosebutton
-  let popupButton = document.createElement('button');
+  const popupButton = document.createElement('button');
   card.appendChild(popupButton);
   popupButton.innerHTML = 'X';
   popupButton.className = 'close_popup';
@@ -222,23 +221,23 @@ function createPopup(id = 0) {
   popupButton.addEventListener('click', closePopup)
   
   // popLanguagues
-  let popupLangs = document.createElement('ul');
+  const popupLangs = document.createElement('ul');
   card.appendChild(popupLangs);
   popupLangs.className = 'projLanguages';
 
   for (let k = 0; k < project[id].langs.length; k += 1) {
-    let popLangsLi = document.createElement('li');
+    const popLangsLi = document.createElement('li');
     popupLangs.appendChild(popLangsLi);
-    let popliButtons = document.createElement('button');
+    const popliButtons = document.createElement('button');
     popLangsLi.appendChild(popliButtons);
     popliButtons.textContent = project[id].langs[k];
   }
   
   // popImg
-  let sortDiv = document.createElement('div');
+  const sortDiv = document.createElement('div');
   sortDiv.className =  'sortDiv';
   card.appendChild(sortDiv);
-  let popupImg = document.createElement('div');
+  const popupImg = document.createElement('div');
   popupImg.className = 'imgDiv'
   sortDiv.appendChild(popupImg);
   if (windowSize.matches) {
@@ -248,29 +247,29 @@ function createPopup(id = 0) {
   }
   
   // poppara
-  let divSort = document.createElement('div');
+  const divSort = document.createElement('div');
   divSort.className = 'divSort';
   sortDiv.appendChild(divSort);
-  let popupPara = document.createElement('p');
+  const popupPara = document.createElement('p');
   divSort.appendChild(popupPara);
   popupPara.textContent = project[id].popPara
   
   // poplastButtons
-  let ulButton = document.createElement('ul');
+  const ulButton = document.createElement('ul');
   divSort.appendChild(ulButton);
   ulButton.className = 'see_buttons';
 
   // Live
-  let liLiveButton = document.createElement('li');
+  const liLiveButton = document.createElement('li');
   ulButton.appendChild(liLiveButton);
   let popLiveButton = document.createElement('button');
   liLiveButton.appendChild(popLiveButton);
   popLiveButton.textContent = 'See Live';
 
   // Source
-  let liSrcButton = document.createElement('li');
+  const liSrcButton = document.createElement('li');
   ulButton.appendChild(liSrcButton);
-  let popSrcButton = document.createElement('button');
+  const popSrcButton = document.createElement('button');
   liSrcButton.appendChild(popSrcButton);
   popSrcButton.textContent = 'See Source';
 }
